6e752a9134b92055e4924ccf5f30df2131a56cbf
A2.2000138 (Nadia Farisatunnisa) <91518693+nadiafarisatunnisa@users.noreply.github.com>
2022-11-17 21:46:59 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'SW-001';
+const CACHE_NAME = "SW-001";
 const toCache = [
   "/",
   "manifest.json",
@@ -14,92 +14,50 @@ const toCache = [
   "assets/js/script.js",
   "index.html",
 ];
-self.addEventListener("beforeinstallprompt", (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
 
+self.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent Chrome 76 and later from showing the mini-infobar
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
   showInstallPromotion();
 });
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(function (cache) {
-      return cache.addAll(toCache)
-    })
-    .then(self.skipWaiting())
-  )
-})
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    fetch(event.request)
-    .catch(() => {
-      return caches.open(CACHE_NAME)
-        .then((cache) => {
-          return cache.match(event.request)
-        })
-    })
-  )
-})
-self.addEventListener('activate', function (event) {
-  event.waitUntil(
-    caches.keys()
-    .then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Hapus cache lama',
-            key)
-          return caches.delete(key)
-        }
-      }))
-    })
-    .then(() => self.clients.claim())
-  )
-})
-// const cacheName = 'SW-001';
-// const ToCache = [
-//   '/',
-//   'manifest.json',
-//   'assets/js/register.js',
-//   'assets/img/icon.png',
-// ];
-// self.addEventListener("beforeinstallprompt", (e) => () => {
-//   e.preventDefault();
-//   deferredPrompt = e;
 
-//   showInstallPromotion();
-// });
-// self.addEventListener('install', function (event) {
-//   event.waitUntil(
-//     caches.open(cacheName)
-//     .then(function (cache) {
-//       return cache.addAll(ToCache);
-//     })
-//     .then(self.skipWaiting())
-//   )
-// })
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     fetch(event.request)
-//     .catch(() => {
-//       return caches.open(cacheName)
-//         .then((cache) => {
-//           return cache.match(event.request)
-//         })
-//     })
-//   )
-// })
-// self.addEventListener('activate', function (event) {
-//   event.waitUntil(
-//     caches.keys()
-//     .then((keyList) => {
-//       return Promise.all(keyList.map((key) => {
-//         if (key !== cacheName) {
-//           console.log('[ServiceWorker] Hapus cache lama',
-//             key)
-//           return caches.delete(key)
-//         }
-//       }))
-//     })
-//     .then(() => self.clients.claim())
-//   )
-// })
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(toCache);
+      })
+      .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request);
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+              console.log("[ServiceWorker] Hapus cache lama", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
